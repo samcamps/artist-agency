b717fa391817284b7ff11f5859de4e44bb9069c9
Samuel Camps <samuel.camps@student.ap.be>
2022-11-13 14:37:20 +0100

    plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../components/layout'
 
@@ -5,6 +6,7 @@ const IndexPage = () => {
   return (
       <Layout pageTitle="Welcome to Inghelbrecht Agency!">
       <p>Lorem ipsum2</p>
+      <StaticImage src="../images/aloha.jpg" alt="griet met paard" />
       </Layout>
   )
 }

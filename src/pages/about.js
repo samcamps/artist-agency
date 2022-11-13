eb91d5faad3a81432514ea836c017f9a92ce5fc0
Samuel Camps <samuel.camps@student.ap.be>
2022-11-13 01:01:57 +0100

    finished react in gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+
+        <Layout pageTitle="About Us">
+
+            <Link to="/">Back to Home</Link>
+
+        </Layout>
+
+    )
+}
+
+export default AboutPage

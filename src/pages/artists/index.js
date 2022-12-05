d6f9b905687f18b11e382086d62cea784f9fc96c
Samuel Camps <samuel.camps@student.ap.be>
2022-12-05 20:27:22 +0100

    programmatisch aanmaken pagina

---
+++ src/pages/artists/index.js
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+
+const ArtistsPage = ({ data: { allWpArtist: { edges } } }) => {
+  return (
+    <Layout pageTitle="Artists of Camps Agency">
+
+      {edges.map((item) => {
+        const artist = item.node.artistMeta;
+        const slug = item.node.slug;
+
+        return <Link to={`/artists/${slug}`}>
+          <p key={item.node.id}>{artist.firstName} {artist.lastName}</p>
+        </Link>
+
+      })}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+  allWpArtist {
+    edges {
+      node {
+        artistMeta {
+          firstName
+          lastName
+          artistName
+        }
+        id
+        slug
+      }
+    }
+  }
+}
+`
+
+export default ArtistsPage

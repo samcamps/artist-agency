ffd3bfcb8b5042bcd1a16558e191e49d28089a4d
Samuel Camps <samuel.camps@student.ap.be>
2022-12-20 21:20:33 +0100

    dynamische images

--- src/pages/artists/{wpArtist.slug}.js
+++ src/pages/artists/{wpArtist.slug}.js
@@ -1,21 +1,27 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../../components/layout'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-const ArtistPage = ({data: {wpArtist: {artistMeta: artist}}}) => {
+const ArtistPage = ({ data: { wpArtist: { artistMeta: artist } } }) => {
+
+  const image = getImage(artist.profilePicture.localFile)
+
+
   return (
     <Layout pageTitle="Artiesten Template">
-    <div>
-      <h3>{artist.artistName}</h3>
-      <h1>{artist.firstName} {artist.lastName}</h1>
-      <div dangerouslySetInnerHTML={{__html: artist.description}} />
-      <p>Email: {artist.email}</p>
-      <p>Phone: {artist.phone}</p>
-      <p>Height: {artist.height}</p>
-      <p>Shirt Size: {artist.shirtSize}</p>
-      <p>Shoe Size: {artist.shoeSize}</p>
-      <p>Origin: {artist.origin}</p>
-    </div>
+      <div>
+        <GatsbyImage image={image} alt={artist.profilePicture.altText} />
+        <h3>{artist.artistName}</h3>
+        <h1>{artist.firstName} {artist.lastName}</h1>
+        <div dangerouslySetInnerHTML={{ __html: artist.description }} />
+        <p>Email: {artist.email}</p>
+        <p>Phone: {artist.phone}</p>
+        <p>Height: {artist.height}</p>
+        <p>Shirt Size: {artist.shirtSize}</p>
+        <p>Shoe Size: {artist.shoeSize}</p>
+        <p>Origin: {artist.origin}</p>
+      </div>
     </Layout>
   )
 }
@@ -34,9 +40,17 @@ export const query = graphql`
         phoneNumber
         shirtSize
         shoeSize
+        profilePicture {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+        }
+        altText
       }
     }
   }
+}
 `
 
 export default ArtistPage

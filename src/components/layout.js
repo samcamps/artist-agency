45756f654676340a0e2bcd666341463ed7c33c86
Samuel Camps <samuel.camps@student.ap.be>
2023-01-01 16:23:07 +0100

    contact page

--- src/components/layout.js
+++ src/components/layout.js
@@ -52,6 +52,11 @@ const Layout = ({ children }) => {
               Artists
             </Link>
           </li>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/contact">
+              Contact
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>{children}</main>
